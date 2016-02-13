edd93885c92579d7411f5f4ff29db5a804a3ee42
Anis Ahmad <anis.programmer@gmail.com>
2016-02-14 02:19:38 +0600

    Cleaned up non required files and debugging things

--- Tests/app/AppKernel.php
+++ Tests/app/AppKernel.php
@@ -4,7 +4,6 @@ namespace Ajaxray\GulpBusterBundle\Tests\app;
 use Ajaxray\GulpBusterBundle\GulpBusterBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
@@ -38,16 +37,5 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config.yml');
-    }
-
-    public function indexAction()
-    {
-        $twig = $this->getContainer()->get('twig');
-        $twig->setLoader(new \Twig_Loader_Array([
-            'test_twig_content' => '{{"/js/common.min.js"|with_buster_hash }}',
-            'another_twig_content' => '{{"/js/common.min.css"|with_buster_hash }}'
-        ]));
-
-        return new Response($twig->render('test_twig_content'));
     }
 }
